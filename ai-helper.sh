27dd489fe958445d60fc3457c2d3a134c191fae6
alex <devkral@web.de>
2013-08-07 17:18:33 +0200

    implement index and search (words in insteps only, show only first hit), add po stubs for testing purposes

--- ai-helper.sh
+++ ai-helper.sh
@@ -17,9 +17,12 @@ LAST_STEP=10
 
 script_dir="$(realpath "$(dirname "$0")")"
 
+##
+LANG=en
 
-###hacky, cleaner way needed
-if [ ! -f "$ai_step_file" ] || [[ "$(cat "$ai_step_file")" = "" ]] || [[ "$(cat "$ai_step_file")" = "-1" ]]; then
+
+###create file if it doesn't exist
+if [ ! -f "$ai_step_file" ] || [[ "$(cat "$ai_step_file")" = "" ]]; then
   echo 1 > "$ai_step_file"
 fi
 
@@ -33,13 +36,14 @@ export TEXTDOMAIN="archinstall_helper"
 
 help()
 {
-	echo "Welcome to the step by step install guide"
-	echo "Usage:"
-	echo "  next: continue one step forwards"
-	echo "  back: go one step back"
-	echo "  repeat: repeat current step"
-	echo "  reset: begin from begin"
-	echo "  set <step>: go to step"
+  echo "Welcome to the step by step install guide"
+  echo "Usage:"
+  echo "  next: continue one step forwards"
+  echo "  back: go one step back"
+  echo "  repeat: repeat current step"
+  echo "  reset: begin from begin"
+  echo "  index: index with topics"
+  echo "  set <step>: go to step"
 }
 
 ############################
@@ -47,16 +51,16 @@ help()
 # $1 for text, $2 for text which wont be put into gettext
 print_text()
 {
-	text_o="$1:\n$(gettext -s "$1")"
-	if [ "$2" != "" ]; then
-	  text_o="${text_o}\n---\n$2"
-	fi
+  text_o="$1:\n$(gettext -s "$1")"
+  if [ "$2" != "" ]; then
+   text_o="${text_o}\n---\n$2"
+  fi
 	
-	if [[ "$(echo "$text_o" | wc -l)" -le "$MAX_LINES"  ]] && [[ "$(echo "$text_o" | wc -L)" -le "$MAX_LONG_LINE"  ]];then
-		echo -e "$text_o"
-	else 
-		echo -e "$text_o" | less
-	fi
+  if [[ "$(echo "$text_o" | wc -l)" -le "$MAX_LINES"  ]] && [[ "$(echo "$text_o" | wc -L)" -le "$MAX_LONG_LINE"  ]];then
+    echo -e "$text_o"
+  else 
+    echo -e "$text_o" | less
+  fi
 	
 }
 
@@ -65,46 +69,72 @@ print_text()
 
 #######contains name of step for search
 
-declare -A insteps
+#declare -a insteps
 insteps=(
-["prepare"]=1
-["network"]=2
-["partitioning"]=3
-["crypto"]=4
-["lvm"]=5
-["filesystems"]=6
-["mount"]=7
-["install"]=8
-["chroot"]=9
-["fine-tuning"]=10
-["cleanup"]=11
+"prepare"
+"network"
+"partitioning"
+"crypto"
+"lvm"
+"filesystems"
+"mount"
+"install"
+"chroot"
+"fine-tuning"
+"cleanup"
 
 )
 
+# $1 search term echo number or -1
+search()
+{
+  if [ "$1" != "" ]; then
+    for (( tempa=0; tempa<${#insteps[@]}; tempa+=1 )); do
+      if echo "${insteps[$tempa]}" | grep -q "$1"; then
+        echo "$tempa"
+        return 0
+      fi
+    done
+  fi
+  echo "-1"
+  return 2
+}
 
 
+index()
+{
+  for (( tempa=0; tempa<${#insteps[@]}; tempa+=1 )); do
+    echo "$tempa: ${insteps[$tempa]}"
+  done
+}
 
-#gruelsome code
+
 #$1 string to verify, returns 1 if false else 0 but this doesn't work so fall back to  cur_step_read=-1
 sanitized_input()
 {
-	tmp_verify=$(echo "$1" | grep -o "^-\?[0-9]\+")
-	
-	if [ "$tmp_verify" = "" ]; then
-		echo "Error: No number could be read from input"
-		cur_step_read=-1
-		return 1
-	fi
-	
-	if [[ $tmp_verify -lt 1 || $tmp_verify -gt $LAST_STEP ]] ; then
+  tmp_verify="$(echo "$1" | grep -o "^-\?[0-9]\+")"
 
-		echo "Error: step out of range"
-		cur_step_read=-1
-		return 1
-	fi
+  if [ "$1" = "" ]; then
+    return 3
+  fi
+
+  if [ "$tmp_verify" = "" ]; then
+    tempnew="$(search "$1")"
+    if [ "$tempnew" -gt "0" ]; then
+      echo "$tempnew"
+      return 0
+    fi
+    echo "Error: No valid step could be read from input" >&2
+    return 2
+  fi
 	
-	cur_step_read=$tmp_verify
-	return 0
+  if [[ $tmp_verify -lt 0 || $tmp_verify -gt ${#insteps[@]} ]] ; then
+
+    echo "Error: step out of range" >&2
+    return 1
+  fi
+  echo "$tmp_verify"
+  return 0
 	
 }
 
@@ -112,128 +142,121 @@ sanitized_input()
 #reads file into internal variable
 read_in()
 {
-	sanitized_input "$(cat "$ai_step_file")"
-	if [[ "$cur_step_read" = "-1" ]] ; then
-		echo "Error: Fileinput error"
-		exit 1
-	fi
-	
-	#return $cur_step_read
+  cur_step_read="$(sanitized_input "$(cat "$ai_step_file")")"
+  if [[ "$?" != "0" ]] ; then
+    echo "Error: File error" >&2
+    exit 1
+  fi
+  echo "$cur_step_read"
 }
 
-#writes internal navigation variable into file
+# writes internal navigation variable into file
+# $1 thing to write
 write_in_file()
 {
-	echo "$cur_step_read" > "$ai_step_file"
-	return
+  echo "$1" > "$ai_step_file"
+  return
 }
 
 cur_step()
 {
-	read_in
-	case $cur_step_read in
-		1)print_text "adjustenvironment"
-		help;;
-		2)print_text "network";;
-		3)print_text "partitioning";;
-		4)print_text "cryptsetupopt";;
-		5)print_text "lvmopt";;
-		6)print_text "filesystems";;
-		7)print_text "mount";;
-		8)print_text "install";;
-		9)print_text "chroot";;
-		10)print_text "fine-tuning";;
-		11)print_text "cleanup";;
-		*)echo  "Error: step not implemented";;
-	esac
-	return
+  #read_in
+  case "$(read_in)" in
+    0)print_text "adjustenvironment"
+    help;;
+    1)print_text "network";;
+    2)print_text "partitioning";;
+    3)print_text "cryptsetupopt";;
+    4)print_text "lvmopt";;
+    5)print_text "filesystems";;
+    6)print_text "mount";;
+    7)print_text "install";;
+    8)print_text "chroot";;
+    9)print_text "fine-tuning";;
+    10)print_text "cleanup";;
+  esac
+  return
 	
 }
 
 
 for_step()
 {
-	read_in
-	if [ "$cur_step_read" = "-1" ]; then
-		echo "Error: invalid step."
-		cleanup
-	fi
+  cur_step_read="$(read_in)"
+  if [ "$cur_step_read" = "-1" ]; then
+    echo "Error: invalid step." >&2
+    cleanup
+  fi
 	
-	if [[ $cur_step_read -ge $LAST_STEP ]]; then
-		echo "Error: Already at the end of installation progress"
-		exit 1
-	else
-		((cur_step_read+=1))
-	fi
-	
-	write_in_file
-	cur_step
-	return
+  if [[ $cur_step_read -ge $LAST_STEP ]]; then
+    echo "Error: Already at the end of installation progress" >&2
+    exit 1
+  else
+    ((cur_step_read+=1))
+  fi
+  write_in_file
+  cur_step
+  return
 }
 
 back_step()
 {
-	read_in
-	if [ "$cur_step_read" = "-1" ]; then
-		echo "Error: invalid step."
-		cleanup
-	fi
+  cur_step_read="$(read_in)"
+  if [ "$cur_step_read" = "-1" ]; then
+    echo "Error: invalid step." >&2
+    cleanup
+  fi
 	
 	
-	if [[ $cur_step_read -le 1 ]]; then
-		echo "Error: Already at the begin of installation progress"
-		exit 1
-	else
-		((cur_step_read-=1))
-	fi
-	write_in_file
-	cur_step
-	return
+  if [[ $cur_step_read -le 1 ]]; then
+    echo "Error: Already at the begin of installation progress" >&2
+    exit 1
+  else
+    ((tempa-=1))
+  fi
+  write_in_file "$cur_step_read"
+  cur_step
+  return
 }
 
 
 user_set_step()
 {
-	if [ "$2" = "" ]; then
-		echo "The steps ranges from 1 to $LAST_STEP"
-		exit 1
-	fi
-	
-	sanitized_input "$2"
-	if [[ "$cur_step_read" = "-1" ]] ; then
-		echo "Error: step invalid"
-		exit 1
-	else
-		write_in_file
-		cur_step
-	fi
-	return
+  if [ "$2" = "" ]; then
+    echo "The steps ranges from 0 to $(((${#insteps[@]}-1)))" >&2
+    exit 1
+  fi
+
+  cur_step_read="$(sanitized_input "$2")"
+  if [[ "$cur_step_read" = "-1" ]] ; then
+    echo "Error: step invalid" >&2
+    exit 1
+  else
+    write_in_file "$cur_step_read"
+    cur_step
+  fi
+  return
 }
-index()
-{
-	#for ((count=1; count<=LAST_STEP; count++))
-	#do
-		
-	#done
-}
+
+
 
 
 
 userinput()
 {
-	text_user="$1"
+  text_user="$1"
 	
-	case "$text_user"  in
-		"next")for_step;;
-		"repeat"|"") cur_step;;
-		"back")back_step;;
-		"reset") cleanup;;
-		"index") index;;
-		"set") user_set_step $*;;
-		*) help;;
-	esac
+  case "$text_user"  in
+    "next")for_step;;
+    "repeat"|"") cur_step;;
+    "back")back_step;;
+    "reset") cleanup;;
+    "index") index;;
+    "set") user_set_step $*;;
+    *) help;;
+  esac
 		
-	return
+  return
 	
 }
 
